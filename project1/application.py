aee32faafe8eed7eff7409a074b6970b17ed3218
DADI91 <48893991+DADI91@users.noreply.github.com>
2020-05-16 18:32:52 +0200

    Revert "Revert "project""
    This reverts commit 4201e57742361c25561aaa61f086bd1370a361fe.

--- project1/application.py
+++ project1/application.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import *
 import sqlite3 as sql
 import csv
@@ -9,9 +7,10 @@ app = Flask(__name__)
 app.secret_key = "cle_secret"
 
 
-@app.route("index")
+@app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080, debug=True)
